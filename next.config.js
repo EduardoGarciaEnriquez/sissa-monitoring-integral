c429a646f467b514c39c6504f57cc1b4630ccaf6
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-07-10 20:25:02 -0600

    chore: ⚙ initial configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+  reactStrictMode: true
+}
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/avocado/:path*',
+        destination: '/product/:path*'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
